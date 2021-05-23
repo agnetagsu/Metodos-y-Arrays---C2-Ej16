07ffe1d13ecc81eaef70cf487e439b5832c54727
Agneta <agnetagsu@yahoo.es>
2021-05-23 13:19:47 +0200

    Agregar archivos de proyecto.

---
+++ Metodos y Arrays - C2 Ej16/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Metodos_y_Arrays___C2_Ej16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] miarray;
+            int longitud;
+            int posicion = 0;
+            Boolean capicua = true;
+
+            Console.WriteLine("Introduce la longitud del array:");
+            longitud = Convert.ToInt32(Console.ReadLine());
+            miarray = new int[longitud];
+
+
+            for (int i = 0; i < miarray.Length; i++)
+            {
+                Console.WriteLine("Introduce el número a introducir en la posición {0}:",i);
+                miarray[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+            do
+            {
+
+                if (miarray[posicion] != miarray[miarray.Length-posicion-1])
+                {
+                    capicua = false;
+                }
+                else
+                {
+                    posicion++;
+                }
+
+                //Console.WriteLine(miarray[i]);
+            } while ((capicua) & (posicion < (miarray.Length/2)));
+
+            if (capicua)
+            {
+                Console.WriteLine("El array es capicua");
+            }
+            else
+            {
+                Console.WriteLine("El array no es capicua");
+            }
+        }
+    }
+}
